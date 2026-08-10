790495b83224bda8c80af5f37bb534e2818a6752
andrew1k3 <andrew.jgardner17@gmail.com>
2026-08-10 19:44:12 +1000

    fun testing

--- apps/web/lib/minecraft-client.ts
+++ apps/web/lib/minecraft-client.ts
@@ -1,12 +1,84 @@
-import MinecraftData, { IndexedBlock, IndexedData } from "minecraft-data";
+import { mergeSchema } from "better-auth/db";
+import { error } from "console";
+import MinecraftData, {
+  IndexedBlock,
+  IndexedData,
+  Item,
+  Recipe,
+  RecipeItem,
+  Shape,
+  ShapedRecipe,
+  ShapeRow,
+} from "minecraft-data";
 
 const mcData: IndexedData = MinecraftData(process.env.MC_VERSION!);
 
 function test() {
   console.log(process.env.MC_VERSION!);
-  const stone_block: IndexedBlock = mcData.blocksByName["stone"]!;
-  console.log(stone_block.id, stone_block.displayName);
-  console.log(mcData.blocks[stone_block.id]);
+  const itemName = "glass_bottle";
+  const item: Item = mcData.itemsByName[itemName]!;
+  console.log(item.id, item.displayName);
+  console.log(mcData.items[item.id]);
+  // console.log(mcData.itemsByName["stone"]!);
+
+  const recipes: Recipe[] = mcData.recipes[item.id]!;
+
+  recipes.forEach((recipe: Recipe) => {
+    if ("inShape" in recipe) {
+      const inShape: Shape = recipe.inShape;
+      console.log(
+        inShape.map((shapeRow: ShapeRow) => {
+          return shapeRow.map((recipeItem: RecipeItem) => {
+            if (!recipeItem) {
+              return "";
+            }
+            if (typeof recipeItem === "object") {
+              throw new Error("errorrrrr");
+            }
+            return mcData.items[recipeItem]?.name;
+          });
+        }),
+      );
+    } else {
+      recipe.ingredients.forEach((recipeItem: RecipeItem) => {
+        if (typeof recipeItem === "object") {
+          throw new Error("errorrrrr");
+        }
+        console.log(mcData.items[recipeItem]);
+        // console.log(recipeItem);
+      });
+    }
+  });
+  // const recipesEntries: [string, Recipe[]][] = Object.entries(mcData.recipes);
+
+  // recipesEntries.forEach(([_, recipes]: [string, Recipe[]]) => {
+  // recipes.forEach((recipe: Recipe) => {
+  //   if ("inShape" in recipe) {
+  //     const inShape: Shape = recipe.inShape;
+  //     console.log(
+  //       inShape.map((shapeRow: ShapeRow) => {
+  //         return shapeRow.map((recipeItem: RecipeItem) => {
+  //           if (!recipeItem) {
+  //             return "";
+  //           }
+  //           if (typeof recipeItem === "object") {
+  //             throw new Error("errorrrrr");
+  //           }
+  //           return mcData.items[recipeItem]?.name;
+  //         });
+  //       }),
+  //     );
+  //   } else {
+  //     recipe.ingredients.forEach((recipeItem: RecipeItem) => {
+  //       if (typeof recipeItem === "object") {
+  //         throw new Error("errorrrrr");
+  //       }
+  //       console.log(mcData.items[recipeItem]);
+  //       // console.log(recipeItem);
+  //     });
+  //   }
+  // });
+  // });
 }
 
 test();
